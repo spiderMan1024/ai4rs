b1fd50edcb8ee5f3679b2c385e83ab0814f08936
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-01-02 15:25:12 +0800

    delete starmetric

--- mmrotate/evaluation/metrics/__init__.py
+++ mmrotate/evaluation/metrics/__init__.py
@@ -4,7 +4,6 @@ from .rotated_coco_metric import RotatedCocoMetric
 from .icdar2015_metric import ICDAR2015Metric
 from .coco_metric_sardet_100k import CocoMetricSARDet100k
 from .fair_metric import FAIRMetric
-from .star_metric import STARMetric
 
 __all__ = ['DOTAMetric', 'RotatedCocoMetric', 'ICDAR2015Metric',
-           'CocoMetricSARDet100k', 'FAIRMetric', 'STARMetric']
+           'CocoMetricSARDet100k', 'FAIRMetric']
