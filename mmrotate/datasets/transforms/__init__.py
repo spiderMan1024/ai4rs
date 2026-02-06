83d1785048e597923c38a6c4e02485a1327f40ed
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-02-06 11:14:18 +0800

    add change detection image transforms

--- mmrotate/datasets/transforms/__init__.py
+++ mmrotate/datasets/transforms/__init__.py
@@ -1,11 +1,38 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 from .loading import LoadPatchFromNDArray
 from .transforms import (ConvertBoxType, ConvertMask2BoxType,
                          RandomChoiceRotate, RandomRotate, Rotate,
                          ConvertWeakSupervision, RegularizeRotatedBox)
+from .loading_cd import (MultiImgLoadAnnotations, MultiImgLoadImageFromFile,
+                         MultiImgLoadInferencerLoader,
+                         MultiImgLoadLoadImageFromNDArray)
+from .transforms_cd import (MultiImgAdjustGamma, MultiImgAlbu, MultiImgCLAHE,
+                            MultiImgExchangeTime, MultiImgNormalize, MultiImgPad,
+                            MultiImgPhotoMetricDistortion, MultiImgRandomCrop,
+                            MultiImgRandomCutOut, MultiImgRandomFlip,
+                            MultiImgRandomResize, MultiImgRandomRotate,
+                            MultiImgRandomRotFlip, MultiImgRerange,
+                            MultiImgResize, MultiImgResizeShortestEdge,
+                            MultiImgResizeToMultiple, MultiImgRGB2Gray)
+from .formatting_cd import MultiImgPackSegInputs
 
 __all__ = [
-    'LoadPatchFromNDArray', 'Rotate', 'RandomRotate', 'RandomChoiceRotate',
-    'ConvertBoxType', 'ConvertMask2BoxType', 'ConvertWeakSupervision',
-    'RegularizeRotatedBox'
+    'LoadPatchFromNDArray',
+
+    'Rotate', 'RandomRotate',
+    'RandomChoiceRotate', 'ConvertBoxType', 'ConvertMask2BoxType',
+    'ConvertWeakSupervision', 'RegularizeRotatedBox',
+
+    'MultiImgLoadAnnotations', 'MultiImgLoadImageFromFile',
+    'MultiImgLoadInferencerLoader', 'MultiImgLoadLoadImageFromNDArray',
+
+    'MultiImgAdjustGamma', 'MultiImgAlbu', 'MultiImgCLAHE',
+    'MultiImgExchangeTime', 'MultiImgNormalize', 'MultiImgPad',
+    'MultiImgPhotoMetricDistortion', 'MultiImgRandomCrop',
+    'MultiImgRandomCutOut', 'MultiImgRandomFlip',
+    'MultiImgRandomResize', 'MultiImgRandomRotate',
+    'MultiImgRandomRotFlip', 'MultiImgRerange',
+    'MultiImgResize', 'MultiImgResizeShortestEdge',
+    'MultiImgResizeToMultiple', 'MultiImgRGB2Gray',
+    
+    'MultiImgPackSegInputs',
 ]
