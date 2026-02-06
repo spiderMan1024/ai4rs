5211876ad35f24c6ad9789a7707d2ea286bc6a7d
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-02-06 11:18:17 +0800

    add `CDLocalVisualize`r and `CDLocalVisBackend` for object detection

--- mmrotate/visualization/__init__.py
+++ mmrotate/visualization/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .local_visualizer import RotLocalVisualizer
 from .palette import get_palette
+from .local_visualizer_cd import CDLocalVisualizer
+from .vis_backend_cd import CDLocalVisBackend
 
-__all__ = ['get_palette', 'RotLocalVisualizer']
+__all__ = ['get_palette', 'RotLocalVisualizer', 'CDLocalVisualizer', 'CDLocalVisBackend']
