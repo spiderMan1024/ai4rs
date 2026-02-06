14cb854bfaacdef876d3e9994465334e27461ba2
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-02-06 11:17:19 +0800

    update for 'SiamEncoderDecoder', 'DIEncoderDecoder'

--- mmrotate/models/detectors/__init__.py
+++ mmrotate/models/detectors/__init__.py
@@ -3,6 +3,8 @@ from .h2rbox import H2RBoxDetector
 from .h2rbox_v2 import H2RBoxV2Detector
 from .refine_single_stage import RefineSingleStageDetector
 from .yolo_detector import YOLODetector
+from .siamencoder_decoder import SiamEncoderDecoder
+from .dual_input_encoder_decoder import DIEncoderDecoder
 
 __all__ = ['RefineSingleStageDetector', 'H2RBoxDetector', 'H2RBoxV2Detector',
-           'YOLODetector']
+           'YOLODetector', 'SiamEncoderDecoder', 'DIEncoderDecoder']
