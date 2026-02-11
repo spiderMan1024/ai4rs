d16cc4c561bd9d8d17338a6d515cdd57b7ea4baf
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-02-11 11:29:00 +0800

    add changer ex s50 and s101

--- projects/changer/changer/__init__.py
+++ projects/changer/changer/__init__.py
@@ -1,10 +1,12 @@
 from .interaction_resnet import IA_ResNetV1c
 from .changer import Changer
 from .interaction_layer import ChannelExchange, SpatialExchange, TwoIdentity
+from .interaction_resnest import IA_ResNeSt
 
 __all__ = ['IA_ResNetV1c',
            'Changer',
            'ChannelExchange',
            'SpatialExchange',
            'TwoIdentity',
+           'IA_ResNeSt'
 ]
