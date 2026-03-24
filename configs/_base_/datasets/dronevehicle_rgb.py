0a38317dc7b9a823730b62d4d254a6cba212e54c
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2026-03-24 10:46:10 +0800

    add dronevehicle_ir.py and dronevehicle_rgb.py

---
+++ configs/_base_/datasets/dronevehicle_rgb.py
@@ -0,0 +1,62 @@
+# dataset settings
+dataset_type = 'DroneVehicleDataset'
+data_root = 'data/DroneVehicle/'
+backend_args = None
+
+train_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', backend_args=backend_args),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='CenterCrop', crop_size=(640, 512), clip_object_border=False),
+    dict(type='mmdet.Resize', scale_factor=1.0),
+    dict(
+        type='mmdet.RandomFlip',
+        prob=0.75,
+        direction=['horizontal', 'vertical', 'diagonal']),
+    dict(type='mmdet.PackDetInputs')
+]
+val_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', backend_args=backend_args),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='CenterCrop', crop_size=(640, 512), clip_object_border=False),
+    dict(type='mmdet.Resize', scale_factor=1.0),
+    dict(
+        type='mmdet.PackDetInputs')
+]
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=None,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        modality='rgb',
+        ann_file_rgb='train/trainlabel/',
+        data_prefix=dict(img_rgb_path='train/trainimg/'),
+        filter_cfg=dict(filter_empty_gt=True),
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        modality='rgb',
+        ann_file_rgb='test/testlabel/',
+        data_prefix=dict(img_rgb_path='test/testimg/'),
+        test_mode=True,
+        pipeline=val_pipeline))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='DOTAMetric',
+    metric='mAP',
+    iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+test_evaluator = val_evaluator
